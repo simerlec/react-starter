2f99eefd4f4681fcb17d1d6b7b77934b24c0848c
Simerle Christopher <simerle.c@gmail.com>
2020-06-07 16:23:16 +0200

    update html template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
 const App = () => {
-  return <div>Hello World</div>;
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <div>
+      <p>Hello World</p>
+      <div>Counter: {counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>Inc</button>
+      <button onClick={() => setCounter(counter - 1)}>Dec</button>
+    </div>
+  );
 };
 
 render(<App />, document.getElementById('root'));
