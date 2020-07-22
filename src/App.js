fdb384d2054a5dc387645966a607712f9fdb98c9
Simerle Christopher <simerle.c@gmail.com>
2020-07-22 15:18:05 +0200

    convert to css modules, add image + animation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import { render } from 'react-dom';
+import root from './style.css';
+
+import react_logo from './assets/img/react.png';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
 
   return (
-    <div>
-      <p>Hello World</p>
-      <div>Counter: {counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>Inc</button>
-      <button onClick={() => setCounter(counter - 1)}>Dec</button>
+    <div className={root.container}>
+      <img className={root.logo} src={react_logo} alt="react icon"></img>
+      <p className={root.green}>Hello World</p>
+      <div className={root.greenAndLarge}>Counter: {counter}</div>
+      <div className="buttons">
+        <button onClick={() => setCounter(counter + 1)}>Inc</button>
+        <button onClick={() => setCounter(counter - 1)}>Dec</button>
+      </div>
     </div>
   );
 };
